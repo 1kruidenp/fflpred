86089a8951bc9c2f8f12dafb737be6374f601bd7
Armin Klotz <armin.klotz@hotmail.com>
2021-06-08 15:48:10 +0100

    Split import_data into more separate functions

--- merge_years/import_data.py
+++ merge_years/import_data.py
@@ -39,7 +39,7 @@ def get_full_data(raw_data_path, return_missing_column_list=False):
             column in list(df17.columns):
             consistent_columns.append(column)
 
-    # Creating a dictionary with columns that are only in some years.
+    # Create a dictionary with columns that are only in some years.
     # Key = column name, value = years for which the column exists.
     missing_columns = {}
     for df in dflist:
@@ -71,8 +71,28 @@ def get_full_data(raw_data_path, return_missing_column_list=False):
     complete_data.name = complete_data.name.apply(lambda n: n.lower())
 
     # Correct game weeks for 2020
-    complete_data['GW'] = complete_data['GW'].apply(corrector)
+    complete_data['GW'] = complete_data['GW'].apply(correct_2020)
 
+    # Match player position
+    complete_data = match_position(complete_data, raw_data_path)
+
+    # Sort ascending by player name and kickoff-date and separate kickoff-date and -time
+    complete_data = sort_kickoff(complete_data)
+
+    if return_missing_column_list:
+        return complete_data, missing_columns
+    return complete_data
+
+
+# Function to correct gameweeks for 2020
+def correct_2020(gw):
+    if gw > 38:
+        return gw - 9
+    return gw
+
+
+def match_position(df, raw_data_path):
+    """ Function to match player positions to the full dataframe """
 
     # Import data about player positions
     raw21 = pd.read_csv(raw_data_path + '/2020-21/players_raw.csv',
@@ -86,14 +106,14 @@ def get_full_data(raw_data_path, return_missing_column_list=False):
     raw17 = pd.read_csv(raw_data_path + '/2016-17/players_raw.csv',
                         encoding='utf-8')
 
+    raw_seasons = [raw17, raw18, raw19, raw20, raw21]
+
     position_by_name = {  #Dictionary to map element_type to position
         1: 'GK',
         2: 'DEF',
         3: 'MID',
         4: 'FWD'
     }
-
-    raw_seasons = [raw17, raw18, raw19, raw20, raw21]
 
     for season in raw_seasons:  #Iterate through the different raw files
 
@@ -107,6 +127,7 @@ def get_full_data(raw_data_path, return_missing_column_list=False):
             if row['name'] == 'caglar söyüncü':
                 season['name'].at[i] = 'çaglar söyüncü'
 
+
     #Change the position of the GK Danny Ward to MID so that we have duplicates that we can drop
     #We will change the position after dropping,
     #IMPORTANT DO NOT REMOVE THIS STEP WITHOUT REMOVING THE STEP OF CHANGING THE POSITION BACK AND VICE VERSA
@@ -115,59 +136,61 @@ def get_full_data(raw_data_path, return_missing_column_list=False):
             raw19['position'].at[i] = 'MID'
 
     #Here we split the data frame into the seasons and join the raw positions on the name
-    df17 = complete_data.loc[complete_data['season'] == 17].copy()
-    abc = raw17[['name', 'position']]
-    abc.set_index('name', drop=True, inplace=True)
-    df17 = df17.join(abc, on=['name'])
+    df17 = df.loc[df['season'] == 17].copy()
+    playerpositions = raw17[['name', 'position']]
+    playerpositions.set_index('name', drop=True, inplace=True)
+    df17 = df17.join(playerpositions, on=['name'])
 
-    df18 = complete_data.loc[complete_data['season'] == 18].copy()
-    abc = raw18[['name', 'position']]
-    abc.set_index('name', drop=True, inplace=True)
-    df18 = df18.join(abc, on=['name'])
+    df18 = df.loc[df['season'] == 18].copy()
+    playerpositions = raw18[['name', 'position']]
+    playerpositions.set_index('name', drop=True, inplace=True)
+    df18 = df18.join(playerpositions, on=['name'])
 
-    df19 = complete_data.loc[complete_data['season'] == 19].copy()
-    abc = raw19[['name', 'position']]
-    abc.set_index('name', drop=True, inplace=True)
-    df19 = df19.join(abc, on=['name'])
+    df19 = df.loc[df['season'] == 19].copy()
+    playerpositions = raw19[['name', 'position']]
+    playerpositions.set_index('name', drop=True, inplace=True)
+    df19 = df19.join(playerpositions, on=['name'])
 
-    df20 = complete_data.loc[complete_data['season'] == 20].copy()
-    abc = raw20[['name', 'position']]
-    abc.set_index('name', drop=True, inplace=True)
-    df20 = df20.join(abc, on=['name'])
+    df20 = df.loc[df['season'] == 20].copy()
+    playerpositions = raw20[['name', 'position']]
+    playerpositions.set_index('name', drop=True, inplace=True)
+    df20 = df20.join(playerpositions, on=['name'])
 
-    df21 = complete_data.loc[complete_data['season'] == 21].copy()
-    abc = raw21[['name', 'position']]
-    abc.set_index('name', drop=True, inplace=True)
-    df21 = df21.join(abc, on=['name'])
+    df21 = df.loc[df['season'] == 21].copy()
+    playerpositions = raw21[['name', 'position']]
+    playerpositions.set_index('name', drop=True, inplace=True)
+    df21 = df21.join(playerpositions, on=['name'])
 
     #Reconcatenate the data into a complete dataset
     complete_data = pd.concat([df17, df18, df19, df20, df21])
 
-    complete_data.drop_duplicates(inplace=True)  #Drop the duplicates (Danny Ward and Ben Davies)
+    complete_data.drop_duplicates(
+        inplace=True)  #Drop the duplicates (Danny Ward and Ben Davies)
 
-    for i, row in complete_data.iterrows():  #Change the goalkeeper Danny Ward to the correct position.
+    for i, row in complete_data.iterrows(
+    ):  #Change the goalkeeper Danny Ward to the correct position.
         if row['name'] == 'danny ward' and row['element'] == 105:
             complete_data['position'].at[i] = 'GK'
 
-    # Turn kickoff date into datetime, sort dataframe by name and kickoff date,
-    complete_data.kickoff_time = pd.to_datetime(complete_data.kickoff_time)
-    complete_data = complete_data.sort_values(by=['name', 'kickoff_time'],
-                                              ascending=True)
-    complete_data.reset_index(drop=True, inplace=True)
-    complete_data['kickoff_date'] = complete_data['kickoff_time']\
-                                    .apply(lambda d: d.date())
-    complete_data['kickoff_time'] = complete_data['kickoff_time']\
-                                    .apply(lambda d: d.time())
-
-    complete_data.reset_index(drop = True)
-
-    if return_missing_column_list:
-        return complete_data, missing_columns
     return complete_data
 
+def sort_kickoff(df):
+    """Turn kickoff date into datetime, sort dataframe by name and kickoff date"""
+    df.kickoff_time = pd.to_datetime(df.kickoff_time)
+    df = df.sort_values(by=['name', 'kickoff_time'],
+                                              ascending=True)
+    df.reset_index(drop=True, inplace=True)
+    df['kickoff_date'] = df['kickoff_time']\
+                                .apply(lambda d: d.date())
+    df['kickoff_time'] = df['kickoff_time']\
+                                .apply(lambda d: d.time())
 
-# Function to correct gameweeks for 2020
-def corrector(gw):
-    if gw > 38:
-        return gw - 9
-    return gw
+    df.reset_index(drop=True)
+
+    return df
+
+
+if __name__=="__main__":
+    df, missing_columns = get_full_data('../raw_data', return_missing_column_list=True)
+    print(df.head(5))
+    print(missing_columns)
